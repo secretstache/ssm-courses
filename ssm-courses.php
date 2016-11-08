cc117660a717ce6ac180b7782f32e5d0d0c66296
Rich Staats <rich@secretstache.com>
2016-11-08 15:48:05 -0700

    version bump to test continuous integration

--- ssm-courses.php
+++ ssm-courses.php
@@ -9,7 +9,7 @@
  * Plugin Name: SSM Courses
  * Plugin URI:  http://secretstache.com
  * Description: Enables a Course Custom Post Type.
- * Version:     0.1.0
+ * Version:     0.1.1
  * Author:      Secret Stache Media
  * Author URI:  http://secretstache.com
  * Text Domain: ssm-courses
